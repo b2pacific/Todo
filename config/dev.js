41f16015610e1f4f3099b5c15ecaf89286e644b8
b2pacific <b2pacific@gmail.com>
2020-06-13 18:19:03 +0530

    removed dev

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: mongoURI: "mongodb+srv://prspndy12:prspandey12@cluster0-n2avs.mongodb.net/todoDB?retryWrites=true&w=majority"
-}
-
-// mongoURI: "mongodb+srv://prspndy12:prspandey12@cluster0-n2avs.mongodb.net/todoDB?retryWrites=true&w=majority"
