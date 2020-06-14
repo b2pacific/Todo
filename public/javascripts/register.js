3d036e36559fe62ebd574352899590053c3ed62c
b2pacific <b2pacific@gmail.com>
2020-06-14 22:45:17 +0530

    register script

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,23 +1,6 @@
 $("button").attr("disabled", true);
 const emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-// $("#InputPassword1").on("input", function () {
-//     var pass = $("InputPassword1").val();
-//     if (pass.length < 5) {
-//         $("#InputPassword1").css({
-//             "border-width": "3px",
-//             "border-color": "red"
-//         });
-//         flag = 0;
-//     } else {
-//         $("#InputPassword1").css({
-//             "border-width": "1px",
-//             "border-color": "#ced4da"
-//         });
-//         flag = 1;
-//     }
-// })
-
 $("#InputPassword2, #InputEmail1").on("input", function () {
     var pass = $("#InputPassword1").val();
     var passcon = $("#InputPassword2").val();
